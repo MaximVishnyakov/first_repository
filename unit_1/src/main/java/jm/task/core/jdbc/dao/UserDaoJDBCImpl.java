0d4e36d742d7cb9e9646c474b20b605cff56d045
MaximVishnyakov <91366821+MaximVishnyakov@users.noreply.github.com>
2022-03-28 13:11:01 +0300

    task 1.1.4

--- unit_1/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ unit_1/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -18,15 +18,15 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         try (Statement statement = conn.createStatement()) {
 
-            try {
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS tableusers(id INT not NULL  AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT, PRIMARY KEY ( id))");
-                conn.commit();
-            } catch (BatchUpdateException x) {
-                x.printStackTrace();
-                conn.rollback();
-            }
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS tableusers(id INT not NULL  AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT, PRIMARY KEY ( id))");
+            conn.commit();
 
         } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
     }
@@ -34,16 +34,15 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
         try (Statement statement = conn.createStatement()) {
 
-
-            try {
-                statement.executeUpdate("DROP TABLE IF EXISTS tableusers");
-                conn.commit();
-            } catch (BatchUpdateException x) {
-                x.printStackTrace();
-                conn.rollback();
-            }
+            statement.executeUpdate("DROP TABLE IF EXISTS tableusers");
+            conn.commit();
 
         } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
     }
@@ -52,20 +51,20 @@ public class UserDaoJDBCImpl implements UserDao {
 
         try (PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO  tableusers(name, lastName, age) VALUES (?, ?, ?)")) {
 
-            try {
-                preparedStatement.setString(1, name);
-                preparedStatement.setString(2, lastName);
-                preparedStatement.setByte(3, age);
-                preparedStatement.executeUpdate();
-                conn.commit();
-            } catch (BatchUpdateException x) {
-                x.printStackTrace();
-                conn.rollback();
-            }
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            conn.commit();
 
             System.out.println("User с именем – " + name + " добавлен в базу данных");
 
         } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
     }
@@ -74,17 +73,15 @@ public class UserDaoJDBCImpl implements UserDao {
 
         try (PreparedStatement preparedStatement = conn.prepareStatement("DELETE FROM tableusers WHERE id = ? ")) {
 
-
-            try {
-                preparedStatement.setLong(1, id);
-                preparedStatement.executeUpdate();
-                conn.commit();
-            } catch (BatchUpdateException x) {
-                x.printStackTrace();
-                conn.rollback();
-            }
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
     }
@@ -94,25 +91,24 @@ public class UserDaoJDBCImpl implements UserDao {
 
         try (Statement statement = conn.createStatement()) {
 
+            ResultSet resultSet = statement.executeQuery("SELECT id, name, lastName, age FROM tableusers");
 
-            try {
-                ResultSet resultSet = statement.executeQuery("SELECT id, name, lastName, age FROM tableusers");
-
-                while (resultSet.next()) {
-                    User user = new User();
-                    user.setId(resultSet.getLong("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setLastName(resultSet.getString("lastName"));
-                    user.setAge(resultSet.getByte("age"));
-                    users.add(user);
-                }
-                conn.commit();
-            } catch (BatchUpdateException x) {
-                x.printStackTrace();
-                conn.rollback();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
             }
+            conn.commit();
 
         } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
 
@@ -122,16 +118,15 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Statement statement = conn.createStatement()) {
 
-
-            try {
-                statement.executeUpdate("TRUNCATE TABLE tableusers");
-                conn.commit();
-            } catch (BatchUpdateException x) {
-                x.printStackTrace();
-                conn.rollback();
-            }
+            statement.executeUpdate("TRUNCATE TABLE tableusers");
+            conn.commit();
 
         } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
     }
